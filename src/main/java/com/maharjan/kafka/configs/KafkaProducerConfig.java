f58a7cb7766a086286d244d71774ea866547a4f7
Amit Maharjan <maharjanamit8@gmail.com>
2025-06-14 14:25:38 -0400

    Using java based configuration instead of application.yml.

--- src/main/java/com/maharjan/kafka/configs/KafkaProducerConfig.java
+++ src/main/java/com/maharjan/kafka/configs/KafkaProducerConfig.java
@@ -2,8 +2,17 @@ package com.maharjan.kafka.configs;
 
 import com.maharjan.kafka.utils.Constants;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
@@ -15,5 +24,24 @@ public class KafkaProducerConfig {
     @Bean
     public NewTopic createCustomerTopic() {
         return new NewTopic(Constants.KAFKA_CUSTOMER_TOPIC, 2, (short) 1);
+    }
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
     }
 }
